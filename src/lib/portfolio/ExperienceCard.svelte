cef88566b6033d4bbc7d84822abf5e5a7e33bb01
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2023-09-14 22:51:17 +0530

    update EXPS & Coding

--- src/lib/portfolio/ExperienceCard.svelte
+++ src/lib/portfolio/ExperienceCard.svelte
@@ -6,26 +6,31 @@
 	let projectTech = ['React Js', 'Mongo DB', 'Node Js', 'Express Js', 'Tailwind CSS', 'Vercel'];
 	let colors = ['#9ceaef', '#a2d2ff', '#ffafcc', '#cdb4db', '#80ed99', '#affc41'];
 	export let projectData = {
-		name: 'Creative Dev',
+		position: 'Full Stack Developer',
 		tech: 'React Js, Mongo DB, Node Js, Express Js, Tailwind CSS, Vercel',
 		desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
 		hostedLink: '/',
-		githubLink: '/'
+		githubLink: '/',
+		duration: '3 Months'
 	};
 </script>
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <!-- svelte-ignore a11y-mouse-events-have-key-events -->
 <div
-	
-	class="bg-white border border-primary px-3 py-5 my-4 hover:bg-gradient-to-r hover:from-white hover:to-sky-200 rounded-xl"
+	class="bg-white box border border-primary px-3 py-5 my-4 rounded-xl transition-all duration-200"
 >
-	<h1 class="text-3xl font-bold w-fit px-0">{projectData?.name}</h1>
+	<div class="flex justify-between items-center">
+		<h1 class="text-3xl font-bold w-fit px-0">{projectData?.position}</h1>
+		<p
+			class="text-gray-500 text-md mt-1 font-mono border-l px-2 border-slate-500 bg-slate-200/50 rounded-r-sm"
+		>
+			{projectData?.duration}
+		</p>
+	</div>
 	<div class="flex flex-wrap gap-2 my-2">
 		{#each projectData?.tech.split(',') as item, i}
-			<Badge variant="outline" style="background:{colors[i % colors.length]}" class="capitalize"
-				>{item}</Badge
-			>
+			<Badge variant="outline" class="capitalize border-gray-400 hover:bg-gray-100 hover:border-slate-800 transition-all duration-200">{item}</Badge>
 		{/each}
 	</div>
 	<div class="my-2 mx-1">
@@ -34,7 +39,7 @@
 		</p>
 	</div>
 	<div class="flex gap-2 justify-end">
-		<Button target="_blank" href={projectData?.hostedLink}>
+		<Button target="_blank" href={projectData?.hostedLink} variant="outline" class="border-primary">
 			View Project
 			<ArrowUpRightSquare strokeWidth="1.4" class="ml-2" size="20" />
 		</Button>
@@ -57,17 +62,23 @@
 		z-index: -1;
 		top: 0;
 		bottom: 0;
+		overflow: hidden;
 		left: -0.4em;
-		right: -0.25em;
+		right: -0.3em;
 		border-radius: 0px 10px 10px 0px;
-		background-color: #0fb3ffff;
-		transform-origin: center right;
+		background-color: rgb(255, 234, 73);
+		transform-origin: center left;
 		transform: scaleX(0);
 		transition: transform 0.2s ease-in-out;
 	}
 
 	h1:hover::before {
-		transform: scaleX(1.05);
+		transform: scaleX(1.02);
 		transform-origin: center left;
+	}
+	.box:hover {
+		box-shadow: 10px 10px 0px 0px rgb(26, 180, 252);
+		transform: translate(-7px, -7px);
+		background: rgb(252, 251, 245);
 	}
 </style>
--- src/routes/portfolio/demo/+page.svelte
+++ src/routes/portfolio/demo/+page.svelte
@@ -1,9 +1,29 @@
 <script>
 	import AboutUser from '$lib/portfolio/AboutUser.svelte';
 	import AllProjects from '$lib/portfolio/AllProjects.svelte';
+	import ExperienceCard from '$lib/portfolio/ExperienceCard.svelte';
 	import ProfileCard from '$lib/portfolio/ProfileCard.svelte';
 	import ProjectCard from '$lib/portfolio/ProjectCard.svelte';
 	import SkillsUser from '$lib/portfolio/SkillsUser.svelte';
+	import { BarChart2, FileBox } from 'lucide-svelte';
+	let exps = [
+		{
+			position: 'Frontend Intern',
+			tech: 'React Js, Mongo DB, Node Js, Express Js, Tailwind CSS, Vercel',
+			desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+			hostedLink: '/',
+			githubLink: '/',
+			duration: 'Jan 2023 - Present'
+		},
+		{
+			position: 'Full Stack Developer',
+			tech: 'React Js, Mongo DB, Node Js, Express Js, Tailwind CSS, Vercel',
+			desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+			hostedLink: '/',
+			githubLink: '/',
+			duration: '3 Months'
+		}
+	];
 </script>
 
 <section>
@@ -13,7 +33,23 @@
 	<AboutUser />
 	<SkillsUser />
 	<AllProjects />
-	<ProjectCard />
+	<div class="md:mx-6">
+		<h1 class="text-3xl font-bold flex items-center">
+			<BarChart2 size="30" class="mr-1" />EXPERIENCE
+		</h1>
+		<div id="exp" class="grid grid-cols-1 md:grid-cols-2 gap-8">
+			{#each exps as exp, i}
+				<ExperienceCard projectData={exp} />
+			{/each}
+			<!-- <ExperienceCard /> -->
+		</div>
+	</div>
+	<div class="md:mx-6 mt-3">
+		<h1 class="text-3xl font-bold flex items-center ">
+			<FileBox size="30" class="mr-1" />PROJECTS
+		</h1>
+		<ProjectCard />
+	</div>
 </section>
 
 <style>
